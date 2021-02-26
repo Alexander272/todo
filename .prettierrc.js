779f663acdaffe2d1da6503ba221e25890db9090
Alexander272 <martynovalexander272@gmail.com>
2021-02-26 10:36:20 +0500

    init

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    printWidth: 100,
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    singleQuote: true,
+    trailingComma: 'es5',
+    tabWidth: 4,
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    endOfLine: 'lf',
+    semi: false,
+}
